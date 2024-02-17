077b7093b0ab400796177ad102898530f5b70a8a
Muhamad Pasha Albara <pashaalbara01@gmail.com>
2024-02-17 14:08:51 +0700

    fix: fetch.js

--- frontend/src/fetch.js
+++ frontend/src/fetch.js
@@ -1,17 +1,17 @@
-const container = document.getElementById('container');
-const endpoint = "http://localhost:4000/jawir";
+const container = document.getElementById('container')
+const endpoint = "http://localhost:4000/jawir"
 
 fetch(endpoint)
     .then(res => {
         if (!res.ok) {
-            throw new Error(`HTTP error Status: ${res.status}`);
+            throw new Error(`HTTP error Status: ${res.status}`)
         }
-        return res.json();
+        return res.json()
     })
     .then(data => {
-        const listItems = data.map(item => `<li>${item.name}, Umur: ${item.age}</li>`);
-        container.innerHTML = `<ul>${listItems.join('')}</ul>`;
+        const listItems = data.map(item => `<li>${item.name}, Umur: ${item.age}</li>`)
+        container.innerHTML = `<ul>${listItems.join('')}</ul>`
     })
     .catch(error => {
-        container.innerHTML = `<p>Error: ${error.message}</p>`;
+        container.innerHTML = `<p>Error: ${error.message}</p>`
     });
